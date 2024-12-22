a7cb0caa6eea0215aa8ac9458912f7be4728fb15
Rakibul-Rifat <rakibul.rifat44@gmail.com>
2024-12-22 18:31:27 +0600

    Add files via upload

---
+++ project.js
@@ -0,0 +1,9 @@
+const bar = document.querySelector(".fa-bars");
+const menu = document.querySelector(".menu");
+
+bar.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+});
+
+
+  
